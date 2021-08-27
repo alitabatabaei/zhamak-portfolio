1814b2fdf0da2a2ed421025f15bcd36f9ab04644
Ali Tabatabaei <tabatabaei.a@gmail.com>
2021-08-27 18:48:30 +0430

    added custom Logo & image;

---
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const styles = {
+  width: 150,
+  height: 150,
+  borderRadius: 150,
+  border: '1px solid black',
+}
+const Logo = () => (
+  <img src="/icon.jpg" style={styles} />
+)
+
+export default Logo
